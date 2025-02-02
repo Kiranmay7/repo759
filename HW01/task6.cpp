8bdd183510130e8a2a144f99acdcf0a2940d467d
Kiranmay7 <148510327+Kiranmay7@users.noreply.github.com>
2025-02-02 17:02:08 -0600

    Add files via upload

---
+++ HW01/task6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <number>\n";
+        return 1;
+    }
+    int num = std::atoi(argv[1]);
+    for (int i = 0; i <= num; ++i) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+    for (int i = num; i >= 0; --i) {
+        std::cout << i << " ";
+    }
+    return 0;
+}
